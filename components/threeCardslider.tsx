4cf10a7ccc6c30c09426ca26faf05a314b6fa3e8
nitish <jerrylovee2@gmail.com>
2025-01-21 22:33:32 +0530

    Change | Added Image

--- components/threeCardslider.tsx
+++ components/threeCardslider.tsx
@@ -10,13 +10,13 @@ const rooms = [
     id: 1,
     title: "Super Deluxe with Balcony",
     description: "Enjoy the luxury of our premium rooms, each featuring a private balcony with stunning views. Relax in comfort and style.",
-    image: "/p2.png"
+    image: "/super.jpg"
   },
   {
     id: 2,
     title: "Deluxe Mountain View",
     description: "Experience breathtaking mountain views from our deluxe rooms. Perfect for nature lovers and peaceful retreats.",
-    image: "/p1.png"
+    image: "/remove.jpg"
   },
   {
     id: 3,
